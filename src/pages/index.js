1822bc073bc492aab51de94025db2c41d3003fc5
meniality <patrh_99@hotmail.com>
2020-05-06 15:16:38 -0600

    played with gatsby image but wrapper around image was not sizing correctly. Added regular react img of headshot.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
+import Headshot from "../images/headshot1.png"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Patrick Hoehn" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <img src={Headshot}/>
+    <Link to="/page-3/">Go to page 2</Link>
   </Layout>
 )
 
